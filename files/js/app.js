a21cb80911e24d572af32852044f2a86250593f6
Vitaliy Levin <network_worms@mail.ru>
2023-02-10 22:28:30 +0300

    697.10-02-2023
    Hmm

--- files/js/app.js
+++ files/js/app.js
@@ -1,19 +1,25 @@
 function checkCookies(){
-    let cookieDate = localStorage.getItem('cookieDate');
-    let cooknote = document.querySelector('#cooknote');
-    let cookieBtn = cooknote.querySelector('.cookagree');
-    if( !cookieDate || (+cookieDate + 31536000000) < Date.now() ){
-        cooknote.classList.add('show');
-    }
-    cookieBtn.addEventListener('click', function(){
-        localStorage.setItem( 'cookieDate', Date.now() );
-        cooknote.classList.remove('show');
-    })
+  let cookieDate = localStorage.getItem('cookieDate');
+  let cooknote = document.querySelector('#cooknote');
+  let cookieBtn = cooknote.querySelector('.cookagree');
+  if( !cookieDate || (+cookieDate + 31536000000) < Date.now() ){
+      cooknote.classList.add('show');
+  }
+  cookieBtn.addEventListener('click', function(){
+    localStorage.setItem( 'cookieDate', Date.now() );
+    cooknote.classList.remove('show');
+  })
 }
 checkCookies();
-window.onload = function() {
+window.onload = function(){
   var a = document.title;
-  var text = ":( 410 Gone";
-  console.log(a);
-  console.log(text);
+  var gone = ':( 410 Gone';
+  var t = document.querySelector('title');
+  document.addEventListener('visibilitychange', function(){
+    if(document.visibilityState == 'hidden'){
+      t.innerHTML = gone;
+    } else {
+      t.innerHTML = a;
+    }
+  })
 }
