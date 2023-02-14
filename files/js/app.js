a6e52c35d623d6e0e89aecbfd3f7ddfe5734df42
Vitaliy Levin <network_worms@mail.ru>
2023-02-14 22:14:20 +0300

    706.XD

--- files/js/app.js
+++ files/js/app.js
@@ -2,22 +2,21 @@ function checkCookies(){
   let cookieDate = localStorage.getItem('cookieDate');
   let cooknote = document.querySelector('#cooknote');
   let cookieBtn = cooknote.querySelector('.cookagree');
-  if( !cookieDate || (+cookieDate + 31536000000) < Date.now() ){
+  if( !cookieDate || (+cookieDate + 31536000000) < Date.now()){
       cooknote.classList.add('show');
   }
   cookieBtn.addEventListener('click', function(){
-    localStorage.setItem( 'cookieDate', Date.now() );
+    localStorage.setItem('cookieDate', Date.now());
     cooknote.classList.remove('show');
   })
 }
 checkCookies();
 window.onload = function(){
   var a = document.title;
-  var gone = ':( 410 Gone';
   var t = document.querySelector('title');
   document.addEventListener('visibilitychange', function(){
     if(document.visibilityState == 'hidden'){
-      t.innerHTML = gone;
+      t.innerHTML = ':( 410 Gone';
     } else {
       t.innerHTML = a;
     }
