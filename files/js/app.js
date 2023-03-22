0a667521ca2bdabde7d136fa346c6f07beec6bcf
Vitaliy Levin <network_worms@mail.ru>
2023-03-22 21:57:32 +0300

    715.22-03-2023
    `31 - 22 =` _`9`_ days before _Backup Day_

--- files/js/app.js
+++ files/js/app.js
@@ -1,22 +1,22 @@
+const now = new Date();
 function checkCookies(){
   let cookieDate = localStorage.getItem('cookieDate');
   let cooknote = document.querySelector('#cooknote');
   let cookieBtn = cooknote.querySelector('.cookagree');
-  if( !cookieDate || (+cookieDate + 31536000000) < Date.now()){
+  if( !cookieDate || (+cookieDate + 31536000000) < now){
       cooknote.classList.add('show');
   }
   cookieBtn.addEventListener('click', function(){
-    localStorage.setItem('cookieDate', Date.now());
+    localStorage.setItem('cookieDate', now);
     cooknote.classList.remove('show');
   })
 }
 checkCookies();
 document.addEventListener('DOMContentLoaded', () => {
-  const now = new Date();
   const doc = document.querySelector('html');
   const sleepText = document.querySelector('#sleep');
   const lang = doc.getAttribute('lang');
-  if ((now.getHours () > 22 || now.getHours () < 6)) {
+  if ((now.getHours() > 22 || now.getHours() < 6)) {
     if (lang == 'ru' || lang == 'ru-RU') {
       sleepText.innerHTML = 'Сон полезен не только в играх с открытым миром.<br>';
     } else {
