b3303a12926dbaa1d12a25ee456c97bb0ad001fe
Vitaliy Levin <network_worms@mail.ru>
2023-06-28 00:06:35 +0300

    736.28-06-2023
    {{ include foo.html }} -> {{- include foo.html -}}
    ---
    m3u-gen (revision-3)

--- files/js/m3u-gen.js
+++ files/js/m3u-gen.js
@@ -5,12 +5,20 @@ const nameInput = document.getElementById("listName");
 const inputElement = document.getElementById("input");
 const prefixInput = document.getElementById("prefixInput");
 const playlist = document.getElementById("playlist");
+const sorting = document.getElementById("sorting");
 inputElement.addEventListener("change", handleFiles, true);
 function handleFiles() {
   const rawFileList = [...this.files];
-  rawFileList.sort((a, b) => {
-    return collator.compare(a.name, b.name);
-  });
+  if (sorting.value == 1) {
+    rawFileList.sort((a, b) => {
+      return collator.compare(a.name, b.name);
+    });
+  }
+  if (sorting.value == 2) {
+    rawFileList.sort((a, b) => {
+      return compareNum(a.lastModified, b.lastModified);
+    });
+  }
   const fileList = rawFileList;
   const numFiles = fileList.length;
   for (var i = 0; i < numFiles; i++) {
@@ -24,4 +32,8 @@ function handleFiles() {
   if (nameInput.value != null) { downloadLink.setAttribute("download", nameInput.value + ".m3u"); }
   downloadLink.href = URL.createObjectURL(theBlob);
   playlist.classList.add("show");
+}
+function flush(textInput) { textInput.value = null; }
+function compareNum(a, b) {
+  return a - b;
 }
