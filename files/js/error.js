06c81c5a10586d0c5d60101cef804424d9e53607
Vitaliy Levin <network_worms@mail.ru>
2023-07-15 23:12:24 +0300

    741.desktop
    screamer [update type: bugfix]
    ---
    Go to [Teapot page](https://vitaliklevin.github.io/teapot/) for the new type of horror.

--- files/js/error.js
+++ files/js/error.js
@@ -1,14 +1,26 @@
-var audio = new Audio("/files/audio/huh.wav");
-audio.preload = "auto";
-const audioCtx = new (AudioContext || window.webkitAudioContext)();
+var audioFile = "/files/audio/huh.wav";
+const audioPlay = async url => {
+  const context = new AudioContext();
+  var gainNode = context.createGain();
+  const source = context.createBufferSource();
+  const audioBuffer = await fetch(url)
+    .then(res => res.arrayBuffer())
+    .then(ArrayBuffer => context.decodeAudioData(ArrayBuffer));
+  source.buffer = audioBuffer;
+  source.connect(gainNode);
+  gainNode.connect(context.destination);
+  gainNode.gain.setValueAtTime(1, context.currentTime);
+  source.start();
+};
 document.addEventListener("DOMContentLoaded", () => {
-  const source = audioCtx.createMediaElementSource(audio);
-  source.connect(audioCtx.destination);
-  document.querySelector("#logo").onclick = function() { audio.play(); }
+  if (window.location.hostname != "vitaliklevin.github.io") {
+    audioFile = "https://vitaliklevin.github.io" + audioFile;
+  }
+  document.querySelector("#logo").onclick = () => audioPlay(audioFile);
   window.onkeydown = (e) => {
     if((e.key == "F5") || (e.key.toLowerCase() == "r" && e.ctrlKey)) {
       e.preventDefault();
-      audio.play();
+      audioPlay(audioFile);
     }
   };
 });
