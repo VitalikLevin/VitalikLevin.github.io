27ebc07db60335000e3c7eda38ca33adae023386
Vitaliy Levin <network_worms@mail.ru>
2023-12-15 23:58:28 +0300

    822.15-11-2023
    entire-website
    ---
    Security fix

--- files/js/app.js
+++ files/js/app.js
@@ -59,7 +59,7 @@ function sharePage() {
       text: `Check out Network Worms website`,
       url: document.querySelector('link[rel=canonical]').href
     })
-    .catch((er) => { console.log(`Share error | ${er}`); });
+    .catch((er) => { console.warn(`Share error | ${er}`); });
   }
 }
 function changeTheme() {
@@ -106,7 +106,7 @@ if ('serviceWorker' in navigator) {
     .then(() => navigator.serviceWorker.ready.then((worker) => {
       worker.sync.register('syncdata');
     }))
-    .catch((err) => console.log(`Service worker’s fail | ${err}`));
+    .catch((err) => console.warn(`Service worker’s fail | ${err}`));
 }
 window.addEventListener('beforeinstallprompt', function(e) {
   e.preventDefault();
